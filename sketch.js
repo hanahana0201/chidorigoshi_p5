c23c67416131517b0e0bc69850a81a197b4b098e
Masatoshi Hanai <hanahana0201@gmail.com>
2021-07-27 22:37:20 +0900

    Add push() pop().

--- sketch.js
+++ sketch.js
@@ -1,27 +1,28 @@
 function setup() {
-  createCanvas(500, 500, SVG)
-  background(225)
+  createCanvas(5000, 5000, SVG)
+  background(233, 203, 130)
 }
 
 function draw() {
-  const step = 100
-
+  let step = width / 20
   for (let y = 0; y < height; y += step) {
     for (let x = 0; x < width; x += step) {
-      noStroke()
-      fill(0)
-      translate(x, y)
-      rect(0, 0, step / 2, step / 2)
-      translate(step / 2, step / 2)
-      for (let i = 0; i < 2; i++) {
-        rotate(i * PI)
-        quad(0, 0, step / 2, -step / 2, step / 2, -step / 4, step / 4, 0)
-        triangle(0, -step / 4, 0, -step / 2, step / 4, -step / 2)
-      }
+      push()
+        noStroke()
+        fill(158, 122, 70)
+        translate(x, y)
+        rect(0, 0, step / 2, step / 2)
+        translate(step / 2, step / 2)
+        for (let i = 0; i < 2; i++) {
+          rotate(i * PI)
+          quad(0, 0, step / 2, -step / 2, step / 2, -step / 4, step / 4, 0)
+          triangle(0, -step / 4, 0, -step / 2, step / 4, -step / 2)
+        }
+      pop()
     }
   }
 
-  // save("mySVG.svg");
+  // save("chidorigoshi_01.svg");
   // print("saved svg");
   // noLoop();
 }
